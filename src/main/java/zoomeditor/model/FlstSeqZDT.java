7c1d5f453a41f2bddfbdd526c53ed6114811941f
Barsik-Barboosik <Barsik-Barboosik>
2019-08-03 01:48:29 +0300

    Sequence file (FlstSeqZDT) creation

--- src/main/java/zoomeditor/model/FlstSeqZDT.java
+++ src/main/java/zoomeditor/model/FlstSeqZDT.java
@@ -6,7 +6,7 @@ import main.java.zoomeditor.util.ByteUtils;
 public class FlstSeqZDT {
     public static final String FILE_NAME = "FLST_SEQ.ZDT";
     public static final int FILE_SIZE = 4108;
-    public static final byte[] TYPE_ORDER = ByteUtils.hexStringToByteArray("000102030C14160405060708090B");
+    public static final byte[] TYPE_ORDER = ByteUtils.hexStringToByteArray("000102030C0D14160405060708090B");
     public static final byte[] OPENING_LINE = ByteUtils.hexStringToByteArray("3E3E3E00000000000000000000");
     public static final String EMPTY_LINE = new String(ArrayUtils.makeAndFillArray(13, (byte) 0x00));
     public static final byte[] ENDING_LINE = ByteUtils.hexStringToByteArray("3C3C3C00000000000000000000");
