865036f165aad5398a665dbe742217e589f9d98d
Barsik-Barboosik <Barsik-Barboosik>
2019-09-13 20:21:35 +0300

    Added effect type enum

---
+++ src/main/java/zoomeditor/enums/EffectType.java
@@ -0,0 +1,34 @@
+package main.java.zoomeditor.enums;
+
+public enum EffectType {
+    DYNAMICS((byte) 0x01, "DYNAMICS"),
+    FILTER((byte) 0x02, "FILTER"),
+    G_DRV((byte) 0x03, "GUITAR DRIVE"),
+    B_DRV_0C((byte) 0x0C, "BASS DRIVE"), // MS60B
+    B_DRV_0D((byte) 0x0D, "BASS DRIVE"), // MS60B
+    B_DRV_14((byte) 0x14, "BASS DRIVE"),
+    B_DRV_16((byte) 0x16, "BASS DRIVE"),
+    G_AMP((byte) 0x04, "GUITAR AMP"),
+    B_AMP((byte) 0x05, "BASS AMP"),
+    MODULATION((byte) 0x06, "MODULATION"),
+    SFX((byte) 0x07, "SFX"),
+    DELAY((byte) 0x08, "DELAY"),
+    REVERB((byte) 0x09, "REVERB"),
+    PEDAL((byte) 0x0B, "PEDAL");
+
+    private final byte typeByte;
+    private final String description;
+
+    EffectType(byte typeByte, String description) {
+        this.typeByte = typeByte;
+        this.description = description;
+    }
+
+    public byte getTypeByte() {
+        return typeByte;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
